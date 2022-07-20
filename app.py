65eab8d7274d74ed3d31356b07302d0166dfced1
qasdr <109637893+qasdr@users.noreply.github.com>
2022-07-20 12:22:43 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
+import sqlite3
+
+con = sqlite3.connect('db.db')
+cur = con.cursor()
+
+def login_users(id, pw):
+    cur.execute(f"SELECT * FROM users WHERE id='{id}' and pwd='{pw}'")
+    return cur.fetchone()
 
 menu = st.sidebar.selectbox('MENU', options=['로그인', '회원가입', '회원목록'])
 
 if menu == '로그인':
     st.subheader('로그인')
+    login_id = st.text_input('아이디', placeholder='아이디를 입력하세요')
+    login_pw = st.text_input('비밀번호', placeholder='비밀번호를 입력하세요',type='password')
+
+    login_btn = st.button('로그인')
+    if login_btn:
+        user_info = login_users(login_id, login_pw)
+        st.write(user_info[0])
+        st.write(user_info[1])
+        st.write(user_info[2])
+        st.write(user_info[3])
+        st.write(user_info[4])
     st.sidebar.subheader('로그인')
 if menu == '회원가입':
     st.subheader('회원가입')
